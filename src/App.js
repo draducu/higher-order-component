5aa2749638dec0456814784e02578677a2c1e474
Daniel Raducu <daniel.raducu@intern.arobs.com>
2022-06-30 16:05:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UsersList from "./UsersList";
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <h2>Higher Order Component</h2>
+      <div className="section">
+        <div><UsersList/></div>
+        <div><TodoList/></div>
+      </div>
     </div>
+    
+    </>
   );
 }
 
